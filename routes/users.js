d015cb5caa43fa5749725cdcb0b417a0649651ba
BeTweak <ys@betweak.com>
2024-05-21 10:52:49 +0900

    Create middleware [authenticate]

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/User');
+const {createToken, verifyToken} = require('../utils/auth');
+
+async function authenticate(req, res, next){
+  // Cookie에 있는 authToken을 가져오거나,
+  let token = req.cookies.authToken;
+  // header의 Authorization에 있는 Bearer token을 가져온다.
+  let headerToken = req.headers.authorization;
+  if (!token && headerToken){
+    token = headerToken.split(' ')[1];
+  }
+  token = token.length > 0 ? token : null;
+  const user = verifyToken(token);
+  req.user = user;
+  next();
+}
+
+async function loginRequired(req, res, next){
+  if(!req.user){
+    const error = new Error("login Required.");
+    error.status = 403;
+    next(error);
+  }
+  next();
+}
+
 
 router.post('/signup', async (req, res, next)=>{
   try {
@@ -37,13 +62,14 @@ router.post("/login", async (req, res, next) => {
 });
 
 router.all("/logout", async (req, res, next) => {
+  // cookie 삭제
   try {
     res.cookie("authToken", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
     });
     res.status(204).send();
-    
+
   } catch (err) {
     console.error(err);
     res.status(400);
